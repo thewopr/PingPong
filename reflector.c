791131a4148858611eb1c11bbbee7f8599bd8154
Thomas Krisch <tak017@bucknell.edu>
2010-03-12 10:37:00 -0500

    Finished adding code to reflector, working on writing pingpong to test the functionality of reflector

--- reflector.c
+++ reflector.c
@@ -16,6 +16,7 @@ extern int errno;
 
 #define BUFSIZE 1000
 #define NUMPACKETS 30
+#define MAXDATAGRAMSIZE 65000
 
 int main(int argc, char* argv[]) {
 
@@ -33,8 +34,6 @@ int main(int argc, char* argv[]) {
 	}
 
 	if(strncmp(argv[2],"UDP", 3) == 0 ) {
-		char	buf[BUFSIZE];	     // buffer for data from the server
-
 		struct sockaddr_in cad; // structure to hold an IP address	
 		struct sockaddr_in sad; // structure to hold an IP address	
 		memset((char *)&sad,0,sizeof(sad)); // clear sockaddr structure	
@@ -42,10 +41,9 @@ int main(int argc, char* argv[]) {
 		sad.sin_addr.s_addr = htonl(INADDR_ANY);
 		sad.sin_port = htons((u_short)port);
 
-		int alen;
 		int fsize = sizeof(struct sockaddr);
 		int bytes_expected;
-		int	sd, sd2;		     // socket descriptor			
+		int	sd;		     // socket descriptor			
 		sd = Socket(AF_INET, SOCK_DGRAM, 0);
 
 		Bind(sd, (struct sockaddr *) &sad, sizeof(sad));
@@ -54,16 +52,13 @@ int main(int argc, char* argv[]) {
 		printf("REFLECTOR: UDP Socket created\n");
 		
 		while(1) {
-			
-			Recvfrom(fd, &bytes_expected, sizeof(bytes_expected),0,(struct *sockaddr)&cad, (socklen_t *) &fsize);
+			printf("Reflector awaiting UDP datagrams\n");	
+			Recvfrom(sd, &bytes_expected, sizeof(bytes_expected),0, &cad, (socklen_t *) &fsize);
 			printf("RELFLECTOR: UDP received a packet, expecting another of %d bytes\n", bytes_expected);
 			int *dump = malloc(bytes_expected);
-			Recvfrom(fd, &bytes_expected, sizeof(bytes_expected),0,(struct *sockaddr)&cad, (socklen_t *) &fsize);
-
-
-
-
-		{
+			Recvfrom(sd, dump, sizeof(dump),0,&cad, (socklen_t *) &fsize);
+			printf("REFLECTOR: UDP received a %d size packet\n", bytes_expected);
+		}
 
 	} else if(strncmp(argv[2],"TCP", 3) == 0) {
 
